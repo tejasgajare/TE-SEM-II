c2ee925dadcc7b2725f553fe01a5cfea5fe19f4d
pccoe <pccoe@ubuntu.ubuntu-domain>
2018-01-11 09:50:53 -0500

    New code added

---
+++ SPOS/MACRO/macro.py
@@ -0,0 +1,58 @@
+MNT = {}
+PTAB = {}
+MDT = {}
+
+
+MACF = 0
+with open("code") as f:
+	line = []
+	while True:
+		line = f.readline()
+		#Detect EOF
+		if line == '':
+			break
+
+		line = line.strip('\n').split(' ')
+		
+
+		if line[0] == 'MACRO':
+			MACF = 1
+			continue
+
+		if MACF == 1:
+			MacName = line[0]
+			param = []
+			for i in line:
+				if i[0] == '&':
+					param.append(i)
+
+			MNT[MacName] = {'#PP' : len(param), '#KP' : 0}
+
+			PTAB[MacName] = [i for i in param]
+
+			MACF = 0
+
+			MDT[MacName] = []
+
+			while line[0] != 'MEND':
+				line = f.readline().strip('\n').split(' ')
+				MDT[MacName].append(line)
+			MDT[MacName].pop()
+
+		if line[0] in MNT:
+			MacName = line[0]
+			param = line[1].split(',')
+			
+			IC = []
+			for L in MDT[MacName]:
+				for i in L:
+					
+
+
+
+			print("\nparam = ", param)
+
+				
+print("MNT = ", MNT)
+print("PTAB = ", PTAB)
+print("MDT = ", MDT)
---
+++ SPOS/MACRO/tc1.py
@@ -0,0 +1,4 @@
+ls = ['a', 'b', 'c']
+
+A = {i for i in ls}
+print(A)
